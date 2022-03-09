4d1be5f1bbf3869364bb67a743a0813e9763a659
Julien Deveaux <julien.deveaux@hotmail.com>
2022-03-09 11:07:52 +0100

    ça marche

--- Queen.py
+++
@@ -1,7 +0,0 @@
-class Queen:
-    ligne = 0
-    colonne = 0
-
-    def __init__(self, x, y):
-        self.ligne = x
-        self.colonne = y
--- main.py
+++ main.py
@@ -1,88 +1,50 @@
-from Queen import Queen
+tailleGrille = 15
+lesReines = []
 
-tailleGrille = 4
+def main():
+    backTrack(lesReines.__len__())
+    printPositions()
 
-lesReines = [Queen(0, 0)]
+
+def backTrack(ligne):
+    if lesReines.__len__() == tailleGrille:
+        return True
+    for i in range(0, tailleGrille):
+        if estLibre(i):
+            lesReines.append(i)
+            if backTrack(ligne + 1):
+                return True
+            else:
+                lesReines.remove(i)
+
+
+def estLibre(val):
+    res = True
+    for i in range(0, lesReines.__len__()):
+        if lesReines[i] == val:
+            res = False
+        if val == (lesReines[i] - (lesReines.__len__() - i)) or val == (lesReines[i] + (lesReines.__len__() - i)):
+            res = False
+    return res
+
 
 def printPositions():
-    grille = [[0]*tailleGrille for i in range(tailleGrille)]
+    grille = [[0] * tailleGrille for i in range(tailleGrille)]
     output = ""
+    it = 0
     for i in lesReines:
-        grille[i.colonne][i.ligne] = 1
+        grille[it][i] = 1
+        it = it + 1
 
     for i in range(tailleGrille):
         for j in range(tailleGrille):
             if grille[i][j] == 1:
-                output = output + "♛"
+                output = output + " ♛ "
             else:
-                output = output + "_"
+                output = output + " _ "
         output = output + "\n"
     print(output)
 
-def compute():
-    backTrack(0)
-    printPositions()
 
-def backTrack(ligne):
-    if lesReines.__len__() == tailleGrille:
-        return 1
-
-    for colonne in range(tailleGrille):
-        ajout = True
-        for reine in lesReines:
-            if reine.colonne == colonne:
-                ajout = False
-
-            c = colonne
-            l = ligne
-            d = 0
-            for i in range(tailleGrille):
-                if [l-i, c-i] in lesReines != -1:
-                    ajout = False
-                d = d+1
-
-
-
-
-
-"""def backTrack(ligne, colonne):
-    if lesReines.__len__() == tailleGrille:
-        return 1
-    if colonne > tailleGrille or ligne > tailleGrille:
-        return 0
-
-    for i in lesReines:
-        if i.colonne == colonne:
-            return backTrack(ligne, colonne + 1)
-        if i.ligne == ligne:
-            return backTrack(ligne + 1, 0)
-
-        check1 = i.ligne + ligne
-        check2 = i.colonne + ligne
-        if (ligne - i.ligne) == colonne or (ligne - i.colonne) == colonne:
-            return backTrack(ligne, colonne + 1)  # check si on est sur la diagonale inférieure d'une autre reine
-        else:
-            lesReines.append(Queen(ligne, colonne))
-            return backTrack(ligne+1, 0)
-
-        """"""for j in lesReines:
-            if j.colonne == colonne:
-                return backTrack(ligne, colonne + 1)
-            check11 = ligne - j.ligne
-            check22 = ligne - j.colonne
-            if (j.ligne - ligne) == colonne or (j.colonne - ligne == colonne):
-                return backTrack(ligne, colonne + 1)
-
-        newReine = Queen(ligne, colonne)
-        lesReines.append(newReine)
-        if backTrack(ligne + 1, 0) == 1:
-            break
-        lesReines.pop(lesReines.__len__() - 1)
-        if colonne < tailleGrille:
-            colonne = colonne + 1
-        else:
-            colonne = 0
-            ligne = ligne + 1"""
-
-
-compute()
+if __name__ == "__main__":
+    main()
